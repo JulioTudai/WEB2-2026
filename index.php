1876742de68e9167621c02a514ac807f5c5a4e57
Julio <juliotweter@hotmail.com>
2026-01-10 10:59:35 -0300

    Inicializando proyecto Web 2 - Configuración de entorno

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hola PHP</title>
+</head>
+<body>
+    <?php
+        $titulo = "Hola Mundo!!!";
+        echo "<h1>" . $titulo . "</h1>";
+    ?>
+</body>
+</html>
